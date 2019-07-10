2ce4acec326ddca79eb6ef000a3dcc90dca86ee0
McHaris <harismanaf96@gmail.com>
2019-07-10 18:03:11 +0700

    swagger

--- src/main/java/com/eksad/latihanrest/controller/ProductController.java
+++ src/main/java/com/eksad/latihanrest/controller/ProductController.java
@@ -5,10 +5,13 @@ import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.eksad.latihanrest.dao.BrandDao;
@@ -16,70 +19,82 @@ import com.eksad.latihanrest.dao.ProductDao;
 import com.eksad.latihanrest.model.Brand;
 import com.eksad.latihanrest.model.Product;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
-@RequestMapping("product")
+@RequestMapping(value = "/api/v1")
+@Api(tags = "Product")
 public class ProductController {
 	@Autowired
 	ProductDao productDao;
 	
 	@Autowired
 	BrandDao brandDao;
-	
-	@RequestMapping("getByBrandId/{brandId}")
+	@ApiOperation(
+			  value = "API to retrieve Product data by Brand Id braadd ",
+			  notes = "Return data with JSON Format",
+			  tags = "Get Data API")
+	@GetMapping("getByBrandIdProduct/{brandId}")
 	public List<Product> getByBrandId(@PathVariable Long brandId) {
 		List<Product> result = new ArrayList<Product>();
 		productDao.findByBrandId(brandId).forEach(result::add);
 		return result;
 	}
-	
-	@RequestMapping("getByProductId/{productId}")
-	public List<Product> getByProductId(@PathVariable Long productId) {
-		List<Product> result = new ArrayList<Product>();
-		productDao.findByProductId(productId).forEach(result::add);
-		return result;
-	}
-	
-	
-	@RequestMapping(value= "save", method = RequestMethod.POST)
+	@ApiOperation(
+			  value = "API to insert data Product bradd",
+			  notes = "Return data with JSON Format",
+			  tags = "Data Manipulation API")
+	@PostMapping(value = "saveproduct")
 	public Product save(@RequestBody Product product) {
 		Brand brand = brandDao.findById(product.getBrandId()).orElse(null);
-				if(brand != null) {
-					product.setBrand(brand);
-					return productDao.save(product);
-				}
+		if(brand != null) {
+			product.setBrand(brand);
+			return productDao.save(product);
+		}
 		return null;
-		
 	}
-	
-	@RequestMapping(value = "update/{id}", method = RequestMethod.PUT)
-	public Product update(@RequestBody Product product, @PathVariable Long id) {
+	@ApiOperation(
+			  value = "API to update data product braaddd",
+			  notes = "Return data with JSON Format",
+			  tags = "Data Manipulation API")
+	@PutMapping(value ="updateproduct/{id}")
+	public String update(@RequestBody Product product,@PathVariable Long id) {
 		Product productSelected = productDao.findById(id).orElse(null);
-		if (productSelected != null) {
+		
+		if(productSelected != null) {
 			productSelected.setName(product.getName());
 			productSelected.setBrandId(product.getBrandId());
-			productSelected.setPrice(product.getPrice());
 			productSelected.setBrand(product.getBrand());
-
-			return productDao.save(productSelected);
-		} else {
-			return null;
+			productSelected.setPrice(product.getPrice());
+			
+			productDao.save(productSelected);
+			return "berhasil memperbaharui";
+		}else {
+			return "gagal memperbaharui";
 		}
-
 	}
-	@RequestMapping(value = "delete/{id}", method = RequestMethod.DELETE)
-	public HashMap<String, Object> delete(@PathVariable Long id) {
+	@ApiOperation(
+			  value = "API to delete Product braddd",
+			  notes = "Return data with JSON Format",
+			  tags = "Data Manipulation API")
+	@DeleteMapping(value ="deleteproduct/{id}")
+	public HashMap<String, Object> delete(@PathVariable Long id){
 		HashMap<String, Object> result = new HashMap<String, Object>();
 		productDao.deleteById(id);
-		result.put("message", "berhasil dihapus");
+		result.put("message","Berhasil di hapus");
 		return result;
 	}
 	
-	@RequestMapping(value ="search/{name}", method = RequestMethod.GET)
-	public List<Product> search(@PathVariable String name){
+	@ApiOperation(
+			  value = "API to search data Product by Name braadddd",
+			  notes = "Return data with JSON Format",
+			  tags = "Get Data API")
+	@GetMapping(value ="search/{name}")
+	public List<Product> search(@PathVariable String search){
 		List<Product> result = new ArrayList<Product>();
-		productDao.findByName(name).forEach(result::add);
-
+		productDao.findBySearch(search).forEach(result::add);
+		
 		return result;
 		};
-	
-}
+	}
