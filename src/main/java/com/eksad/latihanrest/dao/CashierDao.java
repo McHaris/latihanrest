f06ceff0f12edbf622417e4c5f6a4101e3ff07fd
McHaris <harismanaf96@gmail.com>
2019-07-14 17:56:20 +0700

    tugas scurity

--- src/main/java/com/eksad/latihanrest/dao/CashierDao.java
+++
@@ -1,9 +0,0 @@
-package com.eksad.latihanrest.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.eksad.latihanrest.model.Cashier;
-
-public interface CashierDao extends CrudRepository<Cashier, Long> {
-		
-}
